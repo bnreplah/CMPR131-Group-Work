f8349914794879de4bee971fc8f334e0b3efcc4a
Itz <itzrodriguez8@gmail.com>
2021-09-11 12:06:01 -0700

    Option A() and Added Employee Class
    Added option A and added methods and fields to the employee class

--- Projects/main/Assignment2/EmployeeList.h
+++ Projects/main/Assignment2/EmployeeList.h
@@ -20,7 +20,86 @@ void OptionC();
 void OptionD();
 void OptionE();
 
+class Employee {
+private:
+    int employee_id;
+    char status;
+    string lastName;
+    string firstName;
+    string startingDate;
+    string endingDate;
 
+
+public:
+
+    Employee() {
+
+    }
+
+    Employee(Employee&) {
+
+    }
+
+    ~Employee() {
+
+    }
+
+    void input(char inStat, int inEmpID, string inLast, string inFirst, string inStartDat, string inEndDat) {
+        status = inStat;
+        employee_id = inEmpID;
+        lastName = inLast;
+        firstName = inFirst;
+        startingDate = inStartDat;
+        endingDate = inEndDat;
+    }
+
+    int getId() const {
+        return employee_id;
+    }
+
+    char getStatus() const {
+        return status;
+    }
+
+    string getLastName() const {
+        return lastName;
+    }
+
+    string getFirstName() const {
+        return firstName;
+    }
+
+    string getStartingDate() const {
+        return startingDate;
+    }
+
+    string getEndingDate() const {
+        return endingDate;
+    }
+
+    void setStatus(char newStat) {
+        status = newStat;
+    }
+
+    void setLastName(string newLastNm) {
+        lastName = newLastNm;
+    }
+
+    void setFirstName(string newFirstNm) {
+        firstName = newFirstNm;
+    }
+
+    void setStartingDate(string newStartDate) {
+        startingDate = newStartDate;
+    }
+
+    void setEndingDate(string newEndDate) {
+        endingDate = newEndDate;
+    }
+
+
+
+};//end employee class
 
 
 
@@ -89,9 +168,23 @@ char subMenuOptions_el() {
 
 void OptionA() {
     
+    //File name will be stored here
     string filename;
+    
+    //This will be used to read the strings to be converted to the poper data types
     string inputFromFile;
-   
+
+    //This will be used to read the line and be broken down to local variables
+    string inputLineFromFile;
+    LinkTList <Employee> list;
+    
+    //These local variables will be used to initalized the Employee class
+    int ID;
+    char stat;
+    string lastNm;
+    string firstNm;
+    string startDate;
+    string endDate;
 
     //Asking for the file name
     cout << "Enter the file name: ";
@@ -103,29 +196,87 @@ void OptionA() {
     //Opening the data file
     fstream dataFile(filename, ios::in);
 
+    //========================================================================
+    //PROBLEM HERE: There's and issue with the delimiting the file line by line
+    //so far it just reads the first line and does not continue to read the next line
+    //========================================================================
+
     //if opened the file successfully then continue
     if (dataFile)
     {
         //Get the line from the file
-        getline(dataFile,inputFromFile ,'\n');
-
+        getline(dataFile,inputLineFromFile ,'\n');    
+        //if opened the file successfully then continue
         while (dataFile)
         {
-            cout << inputFromFile << endl;
+            if (dataFile)
+            {
+                //Get each string delimited by ',' from the file
+                getline(dataFile,inputFromFile ,',');
+                cout <<endl << "----------------------------" << endl;
+               
+                //while it's still content inside run
+                while (dataFile)
+                {
 
-            getline(dataFile, inputFromFile, '\n');
+                    //for each line there should be 6 strings, converted if needed, that will be stored into the employee class
+                    for (int i = 0; i < 6; i++)
+                    {
+                        switch (i)
+                        {
+                        case 0: 
+                            //char status, will grab the char at the beginning of the string which should be either 'A', 'U','D'
+                            stat = inputFromFile.at(0);
+                            cout << "status: "<<stat << endl;
+                            break;
+                        case 1: 
+                            //int employee_id, stoi will convert the string into integer so that it may be properly stored into the employee class
+                            ID = stoi(inputFromFile);
+                            cout << "ID : " << ID << endl;
+                            break;
+                        case 2:
+                            //string lastName , this will directly store the string inot the lastname 
+                            lastNm = inputFromFile;
+                            cout <<"Last Name: "<< lastNm << endl;
+                            break;
+                        case 3:
+                            //string firstName, this will directly store the string inot the firstname 
+                            firstNm = inputFromFile;
+                            cout <<"First Name: "<< firstNm << endl;
+                            break;
+                        case 4:
+                            //string startingDate, this will directly store the string inot the startDate
+                            startDate = inputFromFile;
+                            cout <<"Start Date: "<< startDate << endl;
+                            break;
+                        case 5:
+                            //string endingDate, this will directly store the string inot the endDate
+                            endDate = inputFromFile;
+                            cout << "End Date: "<<endDate << endl;
+                            break;
+                        default:
+                            cout << "ERROR: setting the values in switch case." << endl;
+                            break;
+                        }
+                        getline(dataFile, inputFromFile, ',');
+                    }
+                }
+                dataFile.close();
+            }
+            else
+            {
+                cout << "ERROR: Cannot opent the file" << endl;
+            }
+           
+            //The Employee Objects would be initialized here and inputted into a list
 
+        getline(dataFile,inputLineFromFile ,'\n');    
         }
-
-        dataFile.close();
-
     }
     else
     {
         cout << "ERROR: Cannot opent the file" << endl;
     }
-
-
 
 }
 //end of OptionA()
@@ -144,83 +295,3 @@ void OptionE() {
 
 }
 
-class Employee {
-private:
-    int employee_id;
-    char status;
-    string lastName;
-    string firstName;
-    string startingDate;
-    string endingDate;
-
-
-public:
-
-    Employee() {
-
-    }
-    
-    Employee(Employee&) {
-
-    }
-
-    ~Employee() {
-
-    }
-
-    void input(char inStat,int inEmpID,string inLast,string inFirst, string inStartDat, string inEndDat){
-        status = inStat;
-        employee_id = inEmpID;
-        lastName = inLast;
-        firstName = inFirst;
-        startingDate = inStartDat;
-        endingDate = inEndDat;
-    }
-
-    int getId() const{
-        return employee_id;
-    }
-
-    char getStatus() const {
-        return status;
-    }
-
-    string getLastName() const {
-        return lastName;
-    }
-        
-    string getFirstName() const {
-        return firstName;
-    }
-
-    string getStartingDate() const {
-        return startingDate;
-    }
-
-    string getEndingDate() const {
-        return endingDate;
-    }
-
-    void setStatus(char newStat) {
-        status = newStat;
-    }
-    
-    void setLastName(string newLastNm) {
-        lastName = newLastNm;
-    }
-        
-    void setFirstName(string newFirstNm) {
-        firstName = newFirstNm;
-    }
-
-    void setStartingDate(string newStartDate) {
-        startingDate = newStartDate;
-    }
-        
-    void setStartingDate(string newEndDate) {
-        endingDate = newEndDate;
-    }
-
-
-
-};//end employee class
