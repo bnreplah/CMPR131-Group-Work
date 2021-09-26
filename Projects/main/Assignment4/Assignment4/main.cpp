331e3ce4e1498609ac3674d08030bc0f8c45cde3
Tinman21088 <89434185+Tinman21088@users.noreply.github.com>
2021-09-25 20:17:46 -0700

    updated main.cpp

--- Projects/main/Assignment4/Assignment4/main.cpp
+++ Projects/main/Assignment4/Assignment4/main.cpp
@@ -1,80 +1,92 @@
-// Assignment 4
-// Professor Q
-// CMPR 131 09/20/21
-// Team members:
-// 
-//	
-// 
-// 
+//	Names:
+//		Ben Halpern
+//		Itz Rodriquez
+//		Tony Cheng
+//		Jose Chavez
+//		Thien Nguyen
+//	Professor Quach
+//	CMPR 131
+//	Assignment 4
+//	9/23/21
+//
 // Description: 
 //		All the terms, polynomial, complex, and rational should all be stored within a dynamic datastructure
 // 
 // sample dataset:
-//	Rational:		3/4,	4/8,	7/9,	6/12
-//	Polynomial:		x^2 + 3x,	x^3 - 3x,	3x^5 + 3x^3 - 3x
-//	Complex:		2 + i,	3 - i^2,	4x - i
+//      Rational:		3/4,	4/8,	7/9,	6/12
+//	    Polynomial:		x^2 + 3x,	x^3 - 3x,	3x^5 + 3x^3 - 3x
+//	    Complex:		2 + i,	3 - i^2,	4x - i
 
-
-
-
-
+#pragma once
 
 #include <iostream>
-#include "Polynomials.h"
+#include <vector>
+#include "input.h"
 #include "Complex.h"
 #include "Rational.h"
-#include "input.h"
-//#include "myContainers.h"
+#include "Polynomials.h"
 using namespace std;
 
-
 //prototypes
+int menuOption();
 void Option1();
 void Option2();
+void Option3();
 
-//main driver
-int main() {
+int main()
+{
+    do
+    {
+        switch (menuOption())
+        {
+        case 0: exit(1); break;
+        case (1): Option1(); break;
+        case (2): Option2(); break;
+        case (3): Option3(); break;
+        default: cout << "\t\tERROR - Invalid option. Please re-enter."; break;
+        }
+        cout << "\n";
+        pause();
+    } while (true);
 
-
-	do {
-		
-		//menu
-
-
-		//end menu
-		int option = int();
-		option = inputInteger("\tOption: ", 0, 2);
-		
-		switch (option) {
-		case (0): return;
-		case (1): Option1(); break;
-		case (2): Option2(); break;
-
-		}//end
-
-
-
-	} while (true);
-
-
-
-
+    return EXIT_SUCCESS;
 
 }//end main
 
+int menuOption()
+{
+    header("CMPR131 Chapter 4 - Assignmnet 4 by Thien , Itz, Tony, Jose, and Ben");
+    string options[] = { "\n\t\t1 > Complex Numbers ",
+                         "\n\t\t2 > Rational Numbers",
+                         "\n\t\t3 > Polynomials",
+                         "\n\t" + string(100 , char(196)) +
+                         "\n\t\t0 > exit "
+    };
+    for (string option : options)
+        cout << option;
+    header("");
 
+    int optionInteger = inputInteger("\n\t\tOption: ", 0, 3);
+    clrScrn();
+    return optionInteger;
 
+}//end menuOptions
 
-void Option1() {
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Option driver functions
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+void Option1() 
+{
 
+}
 
-}//end Option1()
+void Option2()
+{
 
+}
 
-void Option2() {
+void Option3()
+{
 
-
-
-
-}//end Option2()
+}
