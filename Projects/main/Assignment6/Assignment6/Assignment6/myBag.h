0a8ffda1d0d2aa401ffba46447be23b26910f3b6
bnreplah <benhalpern121@gmail.com>
2021-10-12 02:50:53 -0700

    finished myBag.h needs comments and display messages

--- Projects/main/Assignment6/Assignment6/Assignment6/myBag.h
+++ Projects/main/Assignment6/Assignment6/Assignment6/myBag.h
@@ -1,94 +1,211 @@
 #pragma once
 #include <iostream>
-#include <string>
 #include <iterator>
-
-
+#include <list>
+#include <string>
+#include "input.h"
 
 /**
 *
 *
 *
 */
-class myBag_Int
+class myBag
 {
 private:
-	int* container = nullptr;
-	size_t capacity = size_t(100);
-	size_t size = size_t();
-	int* next = nullptr;
+
+	list<int>* container = nullptr;													//initiates the list pointer to nullptr
+	size_t capacity = size_t(100);													//holds the maximum size of the bag 
+	size_t size = size_t(0);														//holds the current number of nodes in the list
+	list<int>::iterator cursor;														//the itterator pointing to the current position in the bag
+
 public:
 
-	myBag_Int() {
-		container = new int[capacity];
-		next = container;//set pointer to first value in the array
-	}
+	/// [Default Constructor]
+	/// Precondition: container is uninitialized
+	/// Postcondition: 
+	myBag() {
+		if (container == nullptr) {
+			container = new list<int>();
+			cursor = container->begin();//set pointer to first value in the array
+		}//end if
+	}//end myBag
 	
-	myBag_Int(size_t cap) {
-		capacity = cap;
-		container = new int[capacity];
+
+	/// [Argument Constructor]
+	/// Precondition:  
+	/// Postcondition: 
+	myBag(size_t cap) {
+		if (container == nullptr) {
+			capacity = cap;
+			container = new list<int>(cap);
+			cursor = container->begin();
+		}//end if
+	}//end myBag
+
+	/// Precondition:  
+	/// Postcondition: 
+	myBag(myBag& copy) {
+		this->container = copy.container;
+		this->capacity = copy.capacity;
+		this->size = copy.size;
+		this->cursor = this->container->begin();
+	}//end myBag
+
+	/// Precondition:  
+	/// Postcondition: 
+	myBag(const myBag& copy) {
+		this->container = copy.container;
+		this->capacity = copy.capacity;
+		this->size = copy.size;
+		this->cursor = this->container->begin();
+	}//end myBag
+
+
+	~myBag() {
+		delete container;
 	}
-	//~myBag() {}
 
-	/// [stub]
-	/// Precondition:
-	/// Postcondition:
-	int search() const { return; }
+	/// [const]
+	/// Precondition: cannot be an empty list  
+	/// Postcondition: If nullptr, returns, if found returns pointer, if 
+	list<int>::iterator search(int searchValue) const {
+															//returns if nullptr, might give error
 
+		if (!container->empty()) {															//if the container is not empty
+			for (list<int>::iterator itt = this->container->begin(); itt != this->container->end() ; ++itt) {
+				if (*itt == searchValue) {
+					printf("\nFound %i", *itt);
+					
+					return itt;
+				}//end if
+
+			}//end for
+			printf("\n Value %i not found in my bag", searchValue);
+			return this->container->begin();
+		}//end if
+		//else
+		return this->container->end();																				//might error out since not return type iterator
+	}//end search
+
+	/// Precondition: (size_t) cap is a positive integer that represents the new capacity of the list
+	/// Postcondition: sets the capacity of the list
 	void setCapacity(size_t cap) {
 		this->capacity = cap;
-	}
+	}//end setCapacity
 
+	/// [const]
+	/// Precondition: N/A
+	/// Postcondition: Returns the capacity of the list
 	size_t getCapacity() const {
 		return capacity;
-	}
+	}//end getCapacity
 
+	/// Precondition:
+	/// Postcondition:
 	size_t getSize() const {
 		return size;
-	}
+	}//end getSize
 
-	/// [stub]
-	/// Precondition:
-	/// Postcondition:
+	
+	/// Precondition: the (list<int>) container exists and is not nullptr 
+	/// Postcondition: The list is cleared
 	void clear() { 
-		next = nullptr;
-		delete []container;
-		container = nullptr;
-		container = new int[capacity];
-		next = container;
-	}
+		if (container != nullptr) {
+			container->clear();
+			cursor = container->begin();
+			size = 0;
+		}//end if
+	}//end clear
 
-	/// [stub]
+	/// Precondition: Inserts (int) value into the bag 
+	/// Postcondition:
+	void insert(int value) {
+		if(cursor != this->container->end())
+			this->container->insert(cursor, value);														//if empty, will insert at the begining, otherwise at current position
+		else {
+			this->container->insert(this->container->begin(), value);
+		}
+		if (cursor != this->container->end() && *cursor != this->container->back())														//if the pointer is not at the last value in the list
+			++cursor;
+		else
+			this->next();
+
+		++size;
+
+	}//end insert
+
 	/// Precondition:
 	/// Postcondition:
-	void insert() {
-		/*
-		insert code here
-		*/
-
-		++next;
-
-	}
-
 	void remove() {
+		if (this->size == 1) {																		//if only one value
+			clear();
+		}
+		else if (this->size == 0) {
+			return;
+		}
+		else {//else size > 1
+			if (cursor == this->container->end())
+				this->next();
+			int temp = *cursor;
+			this->next();
+			this->container->remove(temp);
+			--size;
+		}
+	}
 
-		/*
-		insert code here
-		*/
-		if(next != &(container[0]))
-			--next;
+	/// Precondition:
+	/// Postcondition:
+	void next() {
+		if (cursor != this->container->end()) {
+			++cursor;
+			if (cursor == this->container->end())
+				cursor = this->container->begin();
+		}//end if
+		else if (cursor == this->container->end() && this->size != 0)
+			cursor = this->container->begin();
+		else return;
+	}
+
+	list<int>::iterator get() {
+		if (size != 0) {
+			next();
+			return cursor;
+		}
+		return this->container->end();
+	}
+
+	/// Precondition:
+	/// Postcondition:
+	void sort() {
+		this->container->sort();
+	}
+
+	void display() {
+		std::cout << *this;
+	}
+
+	void display(list<int>::iterator disp){
+		if(disp != this->container->end())
+			std::cout << *disp;
 	}
 
 
-	/// [stub]
-	/// Precondition:
-	/// Postcondition:
-	void sort() {}
+	void moveCursor(list<int>::iterator move) {
+		this->cursor = move;
+	}
 
-	/// [stub]
 	/// Precondition:
 	/// Postcondition:
-	void display() {}
+	friend ostream& operator << (ostream& strm, const myBag& out){
+		int count = int();
+		for (list<int>::iterator itt = out.container->begin(); itt != out.container->end(); ++itt) {
+			strm << "\n\t\t["<< count++ <<"]\t---\t"<< *itt;
+		}//end for
+
+		return strm;
+	
+	}
 
 };
 
@@ -96,22 +213,23 @@ public:
 /// Postcondition: displays list main menu
 char myBagMenuOption()
 {
-	string options[] = { "\n\t1> Non-template MyBag of integers",
-						"\n\t" + string(100, char(205)) +
+	std::string options[] = { "\n\t1> Non-template MyBag of integers",
+						"\n\t" + std::string(100, char(205)) +
 						"\n\t\tA> clear",
 						"\n\t\tB> insert",
 						"\n\t\tC> search",
 						"\n\t\tD> remove",
 						"\n\t\tE> sort",
 						"\n\t\tF> display",
-						"\n\t" + string(100 , char(196)) +
+						"\n\t\tG> get",
+						"\n\t" + std::string(100 , char(196)) +
 						"\n\t\t0> return "
 						"\n\t" + string(100, char(205))
 	};
-	for (string option : options)
-		cout << option;
+	for (std::string option : options)
+		std::cout << option;
 
-	char optionChar = inputChar("\n\t\toption ", static_cast<string>("abcdef0"));
+	char optionChar = inputChar("\n\t\toption ", static_cast<string>("abcdefg0"));
 	clrScrn();
 
 	return optionChar;
@@ -124,20 +242,22 @@ char myBagMenuOption()
 // Postcondition: main driver, runs selected function
 void runMyBag()
 {
+	myBag intBag = myBag();
 	do
 	{
 		switch (myBagMenuOption())
 		{
 		case '0': return; break;
-		case 'A':; break;
-		case 'B':; break;
-		case 'C':; break;
-		case 'D':; break;
-		case 'E':; break;
-		case 'F':; break;
-		default: cout << "\t\tERROR - Invalid option. Please re-enter."; break;
+		case 'A': intBag.clear(); break;
+		case 'B':intBag.insert(inputInteger("\nInput a value into the bag: ")); break;
+		case 'C':intBag.search(inputInteger("\nInput a value to search for in the bag: ")); break;
+		case 'D':intBag.remove(); break;
+		case 'E':intBag.sort(); break;
+		case 'F':intBag.display(); break;
+		case 'G':intBag.display(intBag.get()); break;
+		default: std::cout << "\t\tERROR - Invalid option. Please re-enter."; break;
 		}
-		cout << "\n";
+		std::cout << "\n";
 		pause();
 		clrScrn();
 	} while (true);
