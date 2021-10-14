b5ca5bbddbc335d688404930447c92d1b0923193
Thien Nguyen <ironhulk20@yahoo.com>
2021-10-14 10:52:06 -0700

    uslessness

--- Projects/main/Assignment6/Assignment6/Assignment6/myBag.h
+++ Projects/main/Assignment6/Assignment6/Assignment6/myBag.h
@@ -10,7 +10,7 @@
 *
 *
 */
-class myBag
+class myBag_int
 {
 private:
 
@@ -24,45 +24,45 @@ public:
 	/// [Default Constructor]
 	/// Precondition: container is uninitialized
 	/// Postcondition: 
-	myBag() {
+	myBag_int() {
 		if (container == nullptr) {
 			container = new list<int>();
 			cursor = container->begin();//set pointer to first value in the array
 		}//end if
-	}//end myBag
+	}//end myBag_int
 	
 
 	/// [Argument Constructor]
 	/// Precondition:  
 	/// Postcondition: 
-	myBag(size_t cap) {
+	myBag_int(size_t cap) {
 		if (container == nullptr) {
 			capacity = cap;
 			container = new list<int>(cap);
 			cursor = container->begin();
 		}//end if
-	}//end myBag
+	}//end myBag_int
 
 	/// Precondition:  
 	/// Postcondition: 
-	myBag(myBag& copy) {
+	myBag_int(myBag_int& copy) {
 		this->container = copy.container;
 		this->capacity = copy.capacity;
 		this->size = copy.size;
 		this->cursor = this->container->begin();
-	}//end myBag
+	}//end myBag_int
 
 	/// Precondition:  
 	/// Postcondition: 
-	myBag(const myBag& copy) {
+	myBag_int(const myBag_int& copy) {
 		this->container = copy.container;
 		this->capacity = copy.capacity;
 		this->size = copy.size;
 		this->cursor = this->container->begin();
-	}//end myBag
+	}//end myBag_int
 
 
-	~myBag() {
+	~myBag_int() {
 		delete container;
 	}
 
@@ -197,7 +197,7 @@ public:
 
 	/// Precondition:
 	/// Postcondition:
-	friend ostream& operator << (ostream& strm, const myBag& out){
+	friend ostream& operator << (ostream& strm, const myBag_int& out){
 		int count = int();
 		for (list<int>::iterator itt = out.container->begin(); itt != out.container->end(); ++itt) {
 			strm << "\n\t\t["<< count++ <<"]\t---\t"<< *itt;
@@ -211,9 +211,9 @@ public:
 
 /// Precondition: N/A
 /// Postcondition: displays list main menu
-char myBagMenuOption()
+char myBag_intMenuOption()
 {
-	std::string options[] = { "\n\t1> Non-template MyBag of integers",
+	std::string options[] = { "\n\t1> Non-template myBag_int of integers",
 						"\n\t" + std::string(100, char(205)) +
 						"\n\t\tA> clear",
 						"\n\t\tB> insert",
@@ -240,21 +240,21 @@ char myBagMenuOption()
 //////////////////////////
 // Precondition: N/A 
 // Postcondition: main driver, runs selected function
-void runMyBag()
+void runMyBag_int()
 {
-	myBag intBag = myBag();
+	myBag_int intBag = myBag_int();
 	do
 	{
-		switch (myBagMenuOption())
+		switch (myBag_intMenuOption())
 		{
 		case '0': return; break;
 		case 'A': intBag.clear(); break;
-		case 'B':intBag.insert(inputInteger("\nInput a value into the bag: ")); break;
-		case 'C':intBag.search(inputInteger("\nInput a value to search for in the bag: ")); break;
-		case 'D':intBag.remove(); break;
-		case 'E':intBag.sort(); break;
-		case 'F':intBag.display(); break;
-		case 'G':intBag.display(intBag.get()); break;
+		case 'B': intBag.insert(inputInteger("\nInput a value into the bag: ")); break;
+		case 'C': intBag.search(inputInteger("\nInput a value to search for in the bag: ")); break;
+		case 'D': intBag.remove(); break;
+		case 'E': intBag.sort(); break;
+		case 'F': intBag.display(); break;
+		case 'G': intBag.display(intBag.get()); break;
 		default: std::cout << "\t\tERROR - Invalid option. Please re-enter."; break;
 		}
 		std::cout << "\n";
@@ -262,3 +262,148 @@ void runMyBag()
 		clrScrn();
 	} while (true);
 }
+
+
+
+
+
+/////////////////////////////////////////
+//// FOR CHEATING PURPOSES ONLY
+/////////////////////////////////////////
+//`
+//
+//// FILE: bag5.h (part of the namespace main_savitch_chapter6)
+//// TEMPLATE CLASS PROVIDED:
+////   bag<Item> (a collection of items; each item may appear multiple times)
+////
+//// TYPEDEFS for the bag<Item> template class:
+////   bag<Item>::value_type
+////     This is the Item type from the template parameter.
+////     It is the data type of the items in the bag. It may be any 
+////     of the C++ built-in types (int, char, etc.), or a class with a default
+////     constructor, a copy constructor, an assignment
+////     operator, and a test for equality (x == y).
+////
+////   bag<Item>::size_type
+////     This is the data type of any variable that keeps track of how many items
+////     are in a bag
+////
+////   bag<Item>::iterator and bag<Item>::const_iterator
+////     Forward iterators for a bag or a const bag.
+////   
+//// CONSTRUCTOR for the bag<Item> class:
+////   bag( )
+////     Postcondition: The bag is empty.
+////
+//// MODIFICATION MEMBER FUNCTIONS for the bag<Item> class:
+////   size_type erase(const Item& target)
+////     Postcondition: All copies of target have been removed from the bag.
+////     The return value is the number of copies removed (which could be zero).
+////
+////   bool erase_one(const Item& target)
+////     Postcondition: If target was in the bag, then one copy of target has
+////     been removed from the bag; otherwise the bag is unchanged. A true
+////     return value indicates that one copy was removed; false indicates that
+////     nothing was removed.
+////
+////   void insert(const Item& entry) 
+////     Postcondition: A new copy of entry has been inserted into the bag.
+////
+////   void operator +=(const bag& addend) 
+////     Postcondition: Each item in addend has been added to this bag.
+////
+//// CONSTANT MEMBER FUNCTIONS for the bag<Item> class:
+////   size_type count(const Item& target) const 
+////     Postcondition: Return value is number of times target is in the bag.
+////
+////   Item grab( ) const 
+////     Precondition: size( ) > 0.
+////     Postcondition: The return value is a randomly selected item from the bag.
+////
+////   size_type size( ) const 
+////     Postcondition: Return value is the total number of items in the bag.
+////
+//// STANDARD ITERATOR MEMBER FUNCTIONS (provide a forward iterator):
+////   iterator begin( )
+////   const_iterator begin( ) const
+////   iterator end( )
+////   const iterator end( ) const
+////
+//// NONMEMBER FUNCTIONS for the bag<Item> class:
+////   template <class Item>
+////   bag<Item> operator +(const bag<Item>& b1, const bag<Item>& b2) 
+////     Postcondition: The bag returned is the union of b1 and b2.
+////
+//// VALUE SEMANTICS for the bag<Item> class:
+////    Assignments and the copy constructor may be used with bag objects.
+////
+//// DYNAMIC MEMORY USAGE by the bag<Item>: 
+////   If there is insufficient dynamic memory, then the following functions throw
+////   bad_alloc: The constructors, insert, operator +=, operator +, and the
+////   assignment operator.
+//
+//#ifndef MAIN_SAVITCH_BAG5_H
+//#define MAIN_SAVITCH_BAG5_H
+//#include <cstdlib>   // Provides NULL and size_t and NULL
+//#include "node2.h"   // Provides node class
+//
+//namespace main_savitch_6B
+//{
+//	template <class Item>
+//	class bag
+//	{
+//	public:
+//		// TYPEDEFS
+//		typedef std::size_t size_type;
+//		typedef Item value_type;
+//		typedef node_iterator<Item> iterator;
+//		typedef const_node_iterator<Item> const_iterator;
+//
+//		// CONSTRUCTORS and DESTRUCTOR
+//		bag();
+//		bag(const bag& source);
+//		~bag();
+//
+//		// MODIFICATION MEMBER FUNCTIONS
+//		size_type erase(const Item& target);
+//		bool erase_one(const Item& target);
+//		void insert(const Item& entry);
+//		void operator +=(const bag& addend);
+//		void operator =(const bag& source);
+//
+//		// CONST MEMBER FUNCTIONS
+//		size_type count(const Item& target) const;
+//		Item grab() const;
+//		size_type size() const { return many_nodes; }
+//
+//		// FUNCTIONS TO PROVIDE ITERATORS
+//		iterator begin()
+//		{
+//			return iterator(head_ptr);
+//		}
+//		const_iterator begin() const
+//		{
+//			return const_iterator(head_ptr);
+//		}
+//		iterator end()
+//		{
+//			return iterator();
+//		} // Uses default constructor 
+//		const_iterator end() const
+//		{
+//			return const_iterator();
+//		} // Uses default constructor 
+//
+//	private:
+//		node<Item>* head_ptr;        // Head pointer for the list of items
+//		size_type many_nodes;        // Number of nodes on the list
+//	};
+//
+//	// NONMEMBER functions for the bag
+//	template <class Item>
+//	bag<Item> operator +(const bag<Item>& b1, const bag<Item>& b2);
+//}
+//
+//// The implementation of a template class must be included in its header file:
+//#include "bag5.template"
+//#endif
