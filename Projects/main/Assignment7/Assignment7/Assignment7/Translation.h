fffb2b0fd0a52eee1c38c113c1978e9ddd9c39b8
bnreplah <benhalpern121@gmail.com>
2021-10-21 23:18:06 -0700

    finished part

--- Projects/main/Assignment7/Assignment7/Assignment7/Translation.h
+++ Projects/main/Assignment7/Assignment7/Assignment7/Translation.h
@@ -33,19 +33,17 @@ private:
 	int _lastLevelOfPrecendence = int();							//the level or precedence of the last object
 
 	//number stack
-	stack<double> numStack = stack<double>();
-	stack<char> valueStack = stack<char>();
+	//stack<double> numStack = stack<double>();
+	//stack<char> valueStack = stack<char>();
 
 	//symbol stack
 	stack<Operator> symbStack = stack<Operator>();
 	
 	string input = string();
-
+	string fresult = string();
 
 public:
-	PostfixConverter() {
-
-	}
+	PostfixConverter() {}
 	
 	PostfixConverter(string pInput): input(pInput) {	}
 
@@ -61,19 +59,19 @@ public:
 			return 6 ;
 			break;
 		case ('^'):
-			return 5 + _parenthesis_level;
+			return 3 +_parenthesis_level;
 			break;
 		case ('*'):
-			return 4 + _parenthesis_level;
+			return 2 +_parenthesis_level;
 			break;
 		case ('/'):
-			return 3 + _parenthesis_level;
+			return 2 +_parenthesis_level;
 			break;
 		case ('+'):
-			return 2 + _parenthesis_level;
+			return 1 +_parenthesis_level;
 			break;
 		case ('-'):
-			return 1 + _parenthesis_level;
+			return 1 +_parenthesis_level;
 			break;
 		case (')'):
 			_parenthesisEnd = true;
@@ -103,20 +101,28 @@ public:
 	//
 	//
 
-	void translate(string input) 
+	void translate(string pinput) 
 	{
+		this->input = pinput;
+
 		//stack<char> hold;
 		string result = string();//((a+b)*c - 5)
 
 		for (int i = 0; i < input.length(); i++)
 		{
-			if (input.at(i) == ' ')
+			
+			if (input.at(i) == ' ')//skip spaces
 				continue;
 			char temp = input.at(i);
 			//If (the next input is a left parenthesis)
 			// push to the stack
-			if (temp == '(') {	//if( the next input is a left parenthesis )
-								//read the left parenthesis and push it onto the stack.
+			if (priority(temp) == -1) {//if number or other operand
+				result += temp;
+				//result += " ";
+			}
+			else if (temp == '(') {	//if RHS parenthesis push to stack
+				//if( the next input is a left parenthesis )
+				//read the left parenthesis and push it onto the stack.
 
 				//result += " "temp;
 				_parenthesisStart = true;
@@ -129,85 +135,76 @@ public:
 				current.symb = temp;
 				symbStack.push(current);//pushes to stack with precedence
 
-			}
+			}//end else
 
-			//			else if (the next input is a number or other operand)
+			//				else if (the next input is a number or other operand)
 			//				read the operand and write it to the output
-			else if (priority(temp) == -1){//then number or other operand
-				result += temp;
-				result += " ";
-			}
-			else if (priority(temp) != 0 && priority(temp) != -1 && priority(temp) != 6){
+			else if (temp == ')'){
+				_parenthesisEnd = true;
+				
+				--_parenthesis_level;
+				
+				_lookingForClose = false;
+
+				while (!symbStack.empty() && symbStack.top().precedence != 6 && priority(temp) != 0 && priority(temp) != -1) {
+					if (symbStack.top().symb != '(' && symbStack.top().symb != ')') {
+						result += symbStack.top().symb;
+						result += " ";
+					}
+					symbStack.pop();
+				}//end while
+				//if(!symbStack.empty()) symbStack.pop();//pops off the parenthesis
+
+				_parenthesisStart = false;
+				
+			} // && priority(hold.top()) > priority(input[i]));	
+			//(priority(temp) != 0 && priority(temp) != -1 && priority(temp) != 6){// if an operand
+			
+			else {//operator found
 				Operator oper;
 				oper.precedence = priority(temp);
 				oper.symb = temp;
-				if (!symbStack.empty()) {
-					if (temp != ')') {
+				result += " ";
+				//if (temp == ')') {
 
-						do {
-							if (!symbStack.empty() && oper.precedence > symbStack.top().precedence) {//can only put on the stack if higher precendence than one on the stack
-								symbStack.push(oper);
-							}
-							else {
-								result +=  symbStack.top().symb;
-								result += " ";
-								symbStack.pop();
-							}
-						} while (!symbStack.empty() && oper.precedence > symbStack.top().precedence);
+				while(!symbStack.empty() && oper.precedence  <= symbStack.top().precedence){
+					if (symbStack.top().symb != '(' && symbStack.top().symb != ')') {
+						
+						result += symbStack.top().symb;
+						result += " ";
+
 					}
-					else {
-						if (symbStack.top().symb != '(' && symbStack.top().symb != ')') {
-							result += symbStack.top().symb;
-							result += " ";
-							symbStack.pop();
-						}
-					}
-				}
-				else
-					symbStack.push(oper);
-			} // && priority(hold.top()) > priority(input[i]));	
-			else {//if ')'
-				while (!symbStack.empty()) {//if at end of input read
-					result += symbStack.top().symb ;
-					result += " ";
 					symbStack.pop();
-				}//end while
+				
+				}
+				symbStack.push(oper);
 			}
-		}//end for
-
-
-		cout << "\n answer" << result;
-
-
-	}
-				/*else if (symbStack.empty()) {
-					_syntaxError = true;
-					return;
-				}*/
-
-
-		//	_lookingForClose = false;
-		//	//_parenthesisEnd = true;
-		//	--_parenthesis_level;
-		//		
-		//}
-
-		//else if (the next input is one of the operation symbols)
-		//do
-		//	print the top operation and pop it
-		//while none of these three conditions are true:
-		//	(1)the stack becomes empty, or
-		//	(2)The next symbol on the stack is a left parenthesis, or
-		//	(3)the next symbol on the stack is an operation with lowere precendece than the next input symbol
-	
 			
+				
+		}//end for
+		if (_parenthesis_level != 0 && _parenthesisEnd && !_parenthesisStart) {
+			_syntaxError = true;
+			cout << "\nError: unbalanced paranthesis\n";
+		}
+		while (!symbStack.empty()) {//if at end of input read
+			result += symbStack.top().symb;
+			result += " ";
+			symbStack.pop();
+		}//end while
 
-		//else
-		//Read and discard the next input symbol(which should be a right parenthesis)
-		//Print the top operation and pop it; keep printing and popping until the next symbol on the stack is a left parenthesis
-		//If no left parenthesis is encountered, then print an error message indicating unbalanced parenthese, and halt.
-		//Finally, pop the left parenthesis
+		
 
+		this->fresult = result;
+	}
+	
+	//precondition: run translate first
+	//postcondition: returns the result as a string
+	string getResult() {
+		return this->fresult;
+	}
+	string getInput() {
+		return this->input;
+	}
 };
 
 
@@ -221,9 +218,15 @@ void runTranslation()
 		
 	
 		PostfixConverter run;
-		string exp = "a + b*(c^d-e)^(f+g*h)-i";// ab+(cd^ e - )(f  g h * + )^ * i -
-
+		//string exp = "a + b*(c^d-e)^(f+g*h)-i";
+		//string exp = "4.8+3.2-6+1+3.7)-8.1*6";//evaluates but tells user ubalanced parenthesis 
+		string exp = "4.8+3.2-(6+1+3.7)-8.1*6";//evaluates but tells user ubalanced parenthesis 
+		
 		run.translate(exp);
+		run.getResult();
+		header("2> Translation of Arithmetic Expression");
+		cout << "\n\t\tInfix expression: " << run.getInput();
+		cout << "\n\t\tPostfix expressions: " << run.getResult();
 		pause();
 		return;
 	
