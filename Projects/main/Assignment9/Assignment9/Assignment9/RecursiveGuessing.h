89103961c95e87e02acdc9a54192dd5ee981ba07
Itz <itzrodriguez8@gmail.com>
2021-11-14 14:00:08 -0800

    updated some documentation for Assignment 9

--- Projects/main/Assignment9/Assignment9/Assignment9/RecursiveGuessing.h
+++ Projects/main/Assignment9/Assignment9/Assignment9/RecursiveGuessing.h
@@ -16,7 +16,7 @@
 /// 
 /// Reviewed by:	[Name]	:	[Date]
 ///					Ben		:	11/2/21
-///							:
+///					Itz		:	11/14/21
 ///							:
 
 #pragma once
@@ -24,62 +24,59 @@
 #include <time.h>
 #include "input.h" 
 
-
-
-/// Precondition:
-/// Postcondition:
-void RecursiveGuessing(int lowNum, int highNum, int& count)
-{
-	count++;
-	if (lowNum >= highNum)
-	{
-		std::cout << "\n\tYour number must be " << lowNum << " .";
-		std::cout << "\n\tNumber of guesses : " << count << "\n\n";
+/// Precondition: Three parameters must be integer values that provide the range and the initial count of guesses 
+/// Postcondition: Will use a recursion function to binary search the number the user is thinking of
+void RecursiveGuessing(int lowNum, int highNum, int& count){
+	
+	count++; //keeps count of the recursive calls
+	if (lowNum >= highNum){
+		std::cout << "\n\tYour number must be " << lowNum << " ."; // A guess from the program is given
+		std::cout << "\n\tNumber of guesses : " << count << "\n\n"; // the number of current guesses is given
 		return;
 	}
 
-	int midpoint = (lowNum + highNum) / 2;
-	char answer;
+	int midpoint = (lowNum + highNum) / 2; // Will calculate the the midpoint 
+	char answer; 
+
 	string message = string();
-	message =  "\n\tIs your number " + to_string(midpoint) + " ? (Y - Yes or N - No) : ";
-	answer = inputChar(message,char('Y'),char('N'));
+	message =  "\n\tIs your number " + to_string(midpoint) + " ? (Y - Yes or N - No) : "; // ask the user if the midpoint is the number that they're thinking of
+	answer = inputChar(message,char('Y'),char('N')); // User will input whether the the guess is correct,('Y' = yes, 'N' = no)
 
-	switch (toupper(answer))
-	{
-	case 'Y':
-	{
-		std::cout << "\n\tThe number you were thinking of was " << midpoint;
+	switch (answer){
+	case 'Y':{ // if the guess is correct this case will output
+		std::cout << "\n\tThe number you were thinking of was " << midpoint; 
 		std::cout << "\n\tNumber of guesses : " << count;
-	} break;
-	case 'N':
-	{
+	}//end case 'Y' 
+			break;
+	case 'N':{// if the guess is incorrect and user inputs 'N', the following will execute
 		message = "\n\tIs your number larger than " + to_string(midpoint) + " ?(Y - Yes or N - No) :";
-		answer = inputChar(message, char('Y'), char('N'));
-
-		switch (toupper(answer))
-		{
-		case 'Y': RecursiveGuessing(midpoint + 1, highNum, count); break;
-		case 'N': RecursiveGuessing(lowNum, midpoint - 1, count); break;
-		default: std::cout << "\n\tError: Invalid Input\n";
-		}
-	}break;
-	default: std::cout << "\n\tError: Invalid Input\n";
-	}
+		answer = inputChar(message, char('Y'), char('N'));// User will input whether the the guess is correct,('Y' = yes, 'N' = no)
+		
+		switch (answer){
+		case 'Y': RecursiveGuessing(midpoint + 1, highNum, count); break;// User will input whether the the guess is larger than the program's guess,('Y' = yes, 'N' = no)
+		case 'N': RecursiveGuessing(lowNum, midpoint - 1, count); break; // will call the recursive function with the new parameters
+		default: std::cout << "\n\tError: Invalid Input\n"; // to catch any error
+		
+		}//end switch
+	}//end Case 'N'
+			break;
+	default: std::cout << "\n\tError: Invalid Input\n";// to catch any error
+	}// end switch
 } 
 
-/// Precondition:
-/// Postcondition:
+/// Precondition: N/A
+/// Postcondition: Will run a recursive function that will ask a user to think of a number and the program will try to guess in a binary search manner
 void runRecursiveGuessing() {
 
 	header("\n\tRecursive Guessing Game ");
-
-	srand(time(NULL));
-	int countGuesses = 0;
-	int highRange = rand() % 100 + 1;
+	
+	srand(time(NULL)); // this will be used to create a random integer used as the maximum range for the user to guess
+	int countGuesses = 0; // keep track of the guesses the program has given
+	int highRange = rand() % 100 + 1; // will keep the guessing range to maximum of 100 and a minimum of 1
 
 	std::cout << "\n\tThink of a number from 1 to " << highRange << "...\n\n";
 	pause();
 
-	RecursiveGuessing(1, highRange, countGuesses);
+	RecursiveGuessing(1, highRange, countGuesses); // initial call to the recursive function
 
 }
